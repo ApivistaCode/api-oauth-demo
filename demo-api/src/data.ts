24d9aae159e878ea023b01024c25c6ea43ea4fc0
Michael Blakeney <mike.blakeney@terazo.com>
2019-09-30 16:58:20 -0400

    adds fake data generation

---
+++ demo-api/src/data.ts
@@ -0,0 +1,40 @@
+import * as faker from 'faker';
+
+const policy = (agents: Array<any>) => {
+  return {
+    id: faker.random.uuid(),
+    name: faker.commerce.productAdjective() + " " + faker.commerce.product() + " Policy",
+    description: faker.lorem.paragraph(),
+    premium: faker.commerce.price(),
+    agentId: faker.random.arrayElement(agents.map( agent => agent.id))
+  }
+
+}
+
+const agent = () => {
+  return {
+    id: faker.random.uuid(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    region: faker.address.county()
+  }
+}
+
+export const generateData = (quantity) => {
+
+  let data = {
+    policies: [],
+    agents: []
+  };
+
+  for(let i = 0; i < quantity; i++) {
+    data.agents.push(agent())
+  }
+  for(let i=0; i<quantity; i++) {
+    data.policies.push(policy(data.agents))
+  }
+
+  return data;
+}
+
+
--- demo-api/src/index.ts
+++ demo-api/src/index.ts
@@ -3,18 +3,19 @@ import * as jsonServer from 'json-server';
 import * as path from 'path';
 
 import { auth, scopes} from './middleware';
+import { generateData } from './data';
 
 const port = process.env.PORT || 9000;
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router(generateData(10));
 
 const middlewares = jsonServer.defaults();
 
 
 
 server.use(middlewares);
-server.use(auth);
-server.use(scopes);
+//server.use(auth);
+//server.use(scopes);
 server.use('/v1',router);
 server.listen(port, () => {
   console.log("JSON server is listening on port " + port );
